272a299fde97ab2f7000fca6f65d0d80fa4bd125
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2024-10-16 17:24:41 +0200

    a

---
+++ MSOPracticum/Command.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSOPracticum
+{
+    public class Command
+    {
+        public Command() { }
+        public List<string> comp;
+
+        public bool IsValid(string comp)
+        {
+            var v = 0;
+            if (comp.Split(" ").Count() <= 1 || comp.Split().Count() > 3)
+            {
+                return false;
+            }
+            if (comp.Split(" ")[0] == "Move" || comp.Split(" ")[0] == "Repeat")
+            {
+                v = int.Parse(comp.Split(" ")[1]);
+            }
+            if (comp != null) 
+            {
+                if (comp == "Turn left" || comp == "Turn right")
+                { return true; }
+                else if (comp.Split(" ")[0] == "Move")
+                { return true; }
+                else if (comp.Split().Count() < 3)
+                { return false; }
+                else if (comp.Split(" ")[0] == "Repeat"
+                         && comp.Split(" ")[2] == "times"
+                         && int.TryParse(comp.Split(" ")[1], result: out v))
+                { return true; }
+                else { return false; }
+            }
+            else { return false; }
+        }
+    }
+}
--- MSOPracticum/Program.cs
+++ MSOPracticum/Program.cs
@@ -1,7 +1,97 @@
-﻿class Program
+﻿using System.Drawing;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace MSOPracticum
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main()
+        {
+            Command cmd = new();
+            cmd.comp = new List<string>();
+            Point point = new Point();
+            point.X = 0; point.Y = 0;
+            string direction = "east";
+            while (Console.ReadLine() != "End")
+            {
+                string line = Console.ReadLine();
+                if (cmd.IsValid(line))
+                {
+                    cmd.comp.Add(line);
+                }
+                else { Console.WriteLine("Error, invalid command, please try again"); }
+            }
+            for (int i = 0; i < cmd.comp.Count; i++)
+            {
+                if (cmd.comp[i].Split(" ")[0] == "Repeat")
+                {
+                    for (int j = 0; j < int.Parse(cmd.comp[i].Split()[1]); j++)
+                    {
+                        
+                    }
+                }
+                else if (cmd.comp[i].Split(" ")[0] == "Move")
+                {
+                    int val = int.Parse(cmd.comp[i].Split(" ")[1]);
+                    if (direction == "east")
+                    {
+                        point.X += val;
+                    }
+                    else if (direction == "north")
+                    {
+                        point.Y -= val;
+                    }
+                    else if (direction == "west")
+                    {
+                        point.X -= val;
+                    }
+                    else if (direction == "south")
+                    {
+                        point.Y += val;
+                    }
+                }
+                else if (cmd.comp[i].Split(" ")[0] == "Turn" && cmd.comp[i].Split(" ")[1] == "left")
+                {
+                    if (direction == "east")
+                    {
+                        direction = "north";
+                    }
+                    else if (direction == "north")
+                    {
+                        direction = "west";
+                    }
+                    else if (direction == "west")
+                    {
+                        direction = "south";
+                    }
+                    else if (direction == "south")
+                    {
+                        direction = "east";
+                    }
+                }
+                else if (cmd.comp[i].Split(" ")[0] == "Turn" && cmd.comp[i].Split(" ")[1] == "right")
+                {
+                    if (direction == "east")
+                    {
+                        direction = "south";
+                    }
+                    else if (direction == "south")
+                    {
+                        direction = "west";
+                    }
+                    else if (direction == "west")
+                    {
+                        direction = "north";
+                    }
+                    else if (direction == "north")
+                    {
+                        direction = "east";
+                    }
+                }
+                Console.WriteLine(cmd.comp[i]);
+            }
+            Console.WriteLine("End state " + (point.X, point.Y) + " facing " + direction);
+        }
     }
 }
