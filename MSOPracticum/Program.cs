542b8a4dfcdd798f9e6b3efb070f36af43a8aa88
rene14361 <rene14361@outlook.com>
2024-10-16 17:21:52 +0200

    Reader class and basics of Program class

--- MSOPracticum/Program.cs
+++ MSOPracticum/Program.cs
@@ -1,7 +1,38 @@
-﻿class Program
+﻿public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        Console.WriteLine("Welcome to our Programming Learning App!\nPlease enter \u001b[1m1\u001b[0m if you want to import your own commands, or \u001b[1m2\u001b[0m if you want to use example commands.");
+        bool modeSelected = false;
+        while (!modeSelected)
+        {
+            modeSelected = program.ChooseMode();
+        }
+
+    }
+
+    // Method responsible for making the player choose a mode. Starts the corresponding mode and returns a bool denoting whether a mode was selected succesfully.
+    private bool ChooseMode()
+    {
+        string input = Console.ReadLine();
+        int number = 0;
+        int.TryParse(input, out number);
+
+        switch (number)
+        {
+            // temporary implementation
+            case 1:
+                Reader reader = new Reader();
+                return true;
+
+            case 2:
+                // not implemented
+                return true;
+
+            default:
+                Console.WriteLine("Invalid input, please enter \u001b[1m1\u001b[0m or \u001b[1m2\u001b[0m.");
+                return false;
+        }
     }
 }
