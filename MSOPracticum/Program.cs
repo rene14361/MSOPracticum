319fb3500e4de49449fe5c70f289eaa12bf9f603
rene14361 <rene14361@outlook.com>
2024-10-18 01:01:19 +0200

    Merge changes

--- MSOPracticum/Program.cs
+++ MSOPracticum/Program.cs
@@ -6,39 +6,39 @@
         {
             Program program = new Program();
             Console.WriteLine("Welcome to our Programming Learning App!\nPlease enter \u001b[1m1\u001b[0m if you want to import your own commands, or \u001b[1m2\u001b[0m if you want to use example commands.");
-            bool isModeSelected = false;
-            while (!isModeSelected)
-            {
-                isModeSelected = program.ChooseMode();
-            }
+            ChooseMode();
         }
 
-        private bool ChooseMode()
+        private static void ChooseMode()
         {
-            string input = Console.ReadLine();
-            int number = 0;
-            int.TryParse(input, out number);
-
-            switch (number)
+            // this works because a valid input breaks the loop by invoking return
+            while (true)
             {
-                case int i when i > 0 && i < 3:
-                    Start(number);
-                    return true;
+                string input = Console.ReadLine();
+                int number = 0;
+                int.TryParse(input, out number);
 
-                default:
-                    Console.WriteLine("Invalid input, please enter \u001b[1m1\u001b[0m or \u001b[1m2\u001b[0m.");
-                    return false;
+                switch (number)
+                {
+                    case int i when i > 0 && i < 3:
+                        Start(number);
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid input, please enter \u001b[1m1\u001b[0m or \u001b[1m2\u001b[0m.");
+                        break;
+                }
             }
         }
 
-        private void Start(int mode) 
+        private static void Start(int mode) 
         {
             int metrics = AskMetrics();
             Parser parser = new Parser();
             parser.StartParser(mode, metrics);
         }
 
-        private int AskMetrics()
+        private static int AskMetrics()
         {
             Console.WriteLine("Please enter \u001b[1m1\u001b[0m if you want to execute the program normally, \u001b[1m2\u001b[0m if you just want the metrics, or \u001b[1m3\u001b[0m if you want both.");
             // this works because a valid input breaks the loop by invoking return
