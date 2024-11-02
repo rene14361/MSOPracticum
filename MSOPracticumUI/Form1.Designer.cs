b603bd000d85ec84b621f998703e41f81e247a6a
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2024-11-02 10:32:33 +0100

    added start to UI

---
+++ MSOPracticumUI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MSOPracticumUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnMove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnMove
+            // 
+            this.BtnMove.Location = new System.Drawing.Point(238, 57);
+            this.BtnMove.Name = "BtnMove";
+            this.BtnMove.Size = new System.Drawing.Size(75, 23);
+            this.BtnMove.TabIndex = 0;
+            this.BtnMove.Text = "Move";
+            this.BtnMove.UseVisualStyleBackColor = true;
+            this.BtnMove.Click += new System.EventHandler(this.BtnMove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 511);
+            this.Controls.Add(this.BtnMove);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnMove;
+    }
+}
+
