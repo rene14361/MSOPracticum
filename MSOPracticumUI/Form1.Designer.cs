3ed01d182f3b55ea340d9ea4075f1ed06ea42620
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2024-11-02 10:42:20 +0100

    small additions

--- MSOPracticumUI/Form1.Designer.cs
+++ MSOPracticumUI/Form1.Designer.cs
@@ -28,34 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BtnMove = new System.Windows.Forms.Button();
+            this.Codetxtbx = new System.Windows.Forms.TextBox();
+            this.BtnRun = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // BtnMove
+            // Codetxtbx
             // 
-            this.BtnMove.Location = new System.Drawing.Point(238, 57);
-            this.BtnMove.Name = "BtnMove";
-            this.BtnMove.Size = new System.Drawing.Size(75, 23);
-            this.BtnMove.TabIndex = 0;
-            this.BtnMove.Text = "Move";
-            this.BtnMove.UseVisualStyleBackColor = true;
-            this.BtnMove.Click += new System.EventHandler(this.BtnMove_Click);
+            this.Codetxtbx.Location = new System.Drawing.Point(201, 63);
+            this.Codetxtbx.Multiline = true;
+            this.Codetxtbx.Name = "Codetxtbx";
+            this.Codetxtbx.Size = new System.Drawing.Size(295, 246);
+            this.Codetxtbx.TabIndex = 1;
+            // 
+            // BtnRun
+            // 
+            this.BtnRun.Location = new System.Drawing.Point(233, 352);
+            this.BtnRun.Name = "BtnRun";
+            this.BtnRun.Size = new System.Drawing.Size(75, 23);
+            this.BtnRun.TabIndex = 2;
+            this.BtnRun.Text = "Run";
+            this.BtnRun.UseVisualStyleBackColor = true;
+            this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(873, 511);
-            this.Controls.Add(this.BtnMove);
+            this.Controls.Add(this.BtnRun);
+            this.Controls.Add(this.Codetxtbx);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button BtnMove;
+        private System.Windows.Forms.TextBox Codetxtbx;
+        private System.Windows.Forms.Button BtnRun;
     }
 }
 
--- MSOPracticumUI/Form1.cs
+++ MSOPracticumUI/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MSOPracticumUI
@@ -17,14 +10,14 @@ namespace MSOPracticumUI
             InitializeComponent();
         }
 
-        private void BtnMove_Click(object sender, EventArgs e)
-        {
-            
-        }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
