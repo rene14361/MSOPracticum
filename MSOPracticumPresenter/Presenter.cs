59212e9883d3e5eb673ed1d91643fb7555ae5c65
rene14361 <rene14361@outlook.com>
2024-11-06 15:58:40 +0100

    fixed issue

--- MSOPracticumPresenter/Presenter.cs
+++ MSOPracticumPresenter/Presenter.cs
@@ -1,4 +1,14 @@
-﻿namespace MSOPracticumPresenter;
+﻿using System;
+namespace MSOPracticumPresenter;
+
+
+public static class Program
+{
+    static void Main()
+    {
+    }
+}
+
 
 public class Presenter
 {
